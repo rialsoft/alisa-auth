725158fe01ed837b710989ff87d7f293cc6f1a50
rayel <rialsoft@gmail.com>
2026-04-28 18:08:14 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Login from './components/Login.vue';
+import { onMounted } from 'vue';
+
+declare global {
+  interface Window {
+    Telegram: {
+      WebApp: any;
+    };
+  }
+}
+
+onMounted(() => {
+  console.log('window.Telegram.WebApp', window.Telegram.WebApp.initData);
+})
+</script>
+
+<template>
+  <Login></Login>
+</template>
---
+++ src/components/Login.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const year = new Date().getFullYear();
+
+const username = ref(null);
+const password = ref(null);
+</script>
+
+<template>
+    <main class="container">
+        <article>
+            <header style="text-align: center; ">Alisa 2.0</header>
+            <form>
+                <fieldset>
+                    <label>
+                        Username
+                        <input v-model="username" placeholder="Username" autocomplete="off" required/>
+                    </label>
+                    <label>
+                        Password
+                        <input type="password" v-model="password" placeholder="Password" autocomplete="off" required />
+                    </label>
+                </fieldset>
+
+                <button type="submit">Login</button>
+            </form>
+        </article>
+
+        <small style="margin-top: -80px; text-align: center; color: grey;">
+            Copyright &copy; {{ year }}
+        </small>
+  </main>
+</template>
+
+<style>
+.container {
+  display: grid;
+  place-items: center; /* Centers vertically & horizontally */
+  height: 100vh;      /* Container needs a height */
+}
+</style>
